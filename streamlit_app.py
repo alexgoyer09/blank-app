49af2306b209f3696e42bb156cbae6872986b585
alexgoyer09 <alex.goyer09@hotmail.com>
2025-11-18 02:08:52 +0000

    tuto: basics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("🎈 My new Streamlit app")
 st.write(
@@ -12,7 +15,6 @@ st.write(
 This is some _markdown_.
 '''
 
-import pandas as pd
 df = pd.DataFrame({'col1': [1,2,3]})
 df  # 👈 Draw the dataframe
 
@@ -20,8 +22,6 @@ x = 10
 'x', x  # 👈 Draw the string 'x' and then the value of x
 
 # Also works with most supported chart types
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 arr = np.random.normal(1, 1, size=100)
@@ -29,3 +29,15 @@ fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
 fig  # 👈 Draw a Matplotlib chart
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
